d64b26d5efabf25fff955bcdededa3aea9a3b740
Butcher05 <150314921+Butcher05@users.noreply.github.com>
2025-08-13 23:03:31 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 
 # Paths
-save_dir = r"D:/Coding/Python/Code/models"
+save_dir = r"{Save model path}"
 model_path = os.path.join(save_dir, "fingerprint_cnn_model.h5")
 labels_path = os.path.join(save_dir, "class_labels.json")
 
@@ -31,10 +31,11 @@ def preprocess_image(image_path, img_size=(128, 128)):
     return img
 
 # Test with a new image
-test_image_path = r"D:\College\YEAR 3\Main Subjects\Biometrics\fingerprint.jpg"
+test_image_path = r"{Test sample image}"
 image = preprocess_image(test_image_path)
 predictions = model.predict(image)
 predicted_class = class_names[np.argmax(predictions)]
 confidence = np.max(predictions) * 100
 
 print(f"🖼 Predicted Class: {predicted_class} ({confidence:.2f}% confidence)")
+
